c68b359c4db8fa0f3097b9bd5ddc8f29cba8e809
Miri988 <rinitael@gmail.com>
2022-11-24 14:32:27 +0300

    homework task variables

---
+++ HW-PHP-2022-11-23/task-2.php
@@ -0,0 +1,6 @@
+<?php
+//Создайте переменную с текстом 'hello' и переменную с текстом 'world'. С помощью этих переменных и операции сложения строк выведите на экран строку 'hello world'.
+$a=(string) readline ("Enter 1 word: ");
+$b=(string) readline ("Enter 2 word: ");
+$c=$a . ' ' . $b;
+echo readline ("Result: $c");
